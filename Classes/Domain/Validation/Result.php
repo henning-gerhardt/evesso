e040373f1de386e4a1915d29a9e9cbfe1a6bdb7e
Henning Gerhardt <henning.gerhardt@web.de>
2017-11-23 21:25:47 +0100

    Add validation result class

---
+++ Classes/Domain/Validation/Result.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * Copyright notice
+ *
+ * (c) 2017 Henning Gerhardt
+ *
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 3
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+namespace Gerh\Evesso\Domain\Validation;
+
+use InvalidArgumentException;
+
+class Result
+{
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @var bool
+     */
+    private $result;
+
+    /**
+     * Class constructor for setting a validation result.
+     *
+     * @param string $message
+     * @param bool $result
+     * @throws InvalidArgumentException thrown, if result is not a boolean value
+     */
+    public function __construct($message, $result)
+    {
+        $this->message = $message;
+
+        if (\is_bool($result)) {
+            $this->result = \boolval($result);
+        } else {
+            throw new InvalidArgumentException('Given result is not a boolean value!');
+        }
+    }
+
+    /**
+     * Return validation message.
+     *
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * Return validation result.
+     *
+     * @return bool
+     */
+    public function getResult(): bool
+    {
+        return $this->result;
+    }
+}
---
+++ Tests/Unit/Domain/Validation/ResultTest.php
@@ -0,0 +1,84 @@
+<?php
+/*
+ * Copyright notice
+ *
+ * (c) 2017 Henning Gerhardt
+ *
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 3
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+namespace Gerh\Evesso\Domain\Validation;
+
+use InvalidArgumentException;
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+class ResultTest extends UnitTestCase
+{
+
+    /**
+     * @ignore
+     */
+    public function classCanNotEmptyBeInstiated()
+    {
+        $result = new Result();
+
+        $this->assertInstanceOf(Result::class, $result);
+        $this->assertEmpty($result->getMessage());
+        $this->assertTrue($result->getResult());
+    }
+
+    /**
+     * @test
+     */
+    public function classCanBeInstiated()
+    {
+        $result = new Result('', false);
+
+        $this->assertInstanceOf(Result::class, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function validResult()
+    {
+        $expectedMessage = 'valid validation';
+        $expectedResult = true;
+        $result = new Result($expectedMessage, $expectedResult);
+
+        $this->assertEquals($expectedMessage, $result->getMessage());
+        $this->assertEquals($expectedResult, $result->getResult());
+    }
+
+    /**
+     * @test
+     */
+    public function invalidResult()
+    {
+        $expectedMessage = 'invalid validation';
+        $expectedResult = false;
+        $result = new Result($expectedMessage, $expectedResult);
+
+        $this->assertEquals($expectedMessage, $result->getMessage());
+        $this->assertEquals($expectedResult, $result->getResult());
+    }
+
+    /**
+     * @test
+     * @expectedException InvalidArgumentException
+     */
+    public function notBooleanResultThrowsException()
+    {
+        $expectedMessage = 'invalid result';
+        $expectedResult = 'bla';
+        new Result($expectedMessage, $expectedResult);
+    }
+}
